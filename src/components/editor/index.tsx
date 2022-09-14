4e8f63b0d9e2b3afda8f299dcbac3e994a962b48
nuuuri <sbfl125@gmail.com>
2022-09-14 18:55:28 +0900

    [style] Editor width 적용

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,9 +1,18 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
-export default function ToastEditor(props: { onChange: Function }) {
+export default function ToastEditor(props: {
+  width: string;
+  onChange: Function;
+}) {
   const ref = useRef<Editor>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.getRootElement().style.width = props.width;
+    }
+  }, []);
 
   const handleChange = () => {
     if (ref.current) {
--- src/pages/post/write.tsx
+++ src/pages/post/write.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
@@ -5,16 +6,40 @@ export default function PostWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const onChangeTitle = (e: any) => {
+    setTitle(e.target.value);
+  };
+
   const submit = () => {};
 
   return (
-    <div>
-      <input placeholder="제목을 입력하세요." />
-      <ToastEditor onChange={setContent} />
+    <Container>
+      <PostTitle
+        value={title}
+        onChange={onChangeTitle}
+        placeholder="제목을 입력하세요."
+      />
+      <ToastEditor width="80%" onChange={setContent} />
+      {/*   <button>취소</button>
+      <button>미리보기</button> */}
       <button onClick={submit}>등록</button>
-    </div>
+    </Container>
   );
 }
 
 // ToastEditor는 ssr을 지원하지 않기 때문에 dynamic import 해줘야함
 const ToastEditor = dynamic(() => import("components/editor"), { ssr: false });
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const PostTitle = styled.input`
+  width: 80%;
+  height: 40px;
+  border: none;
+  border-bottom: 2px solid #e2e2e2;
+  font-size: 17px;
+`;
